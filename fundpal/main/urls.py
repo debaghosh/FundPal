f60dadabac5aa6263fc9822e320d2da2bf428a43
debaghosh <debanjanaghosh99@gmail.com>
2020-09-30 19:04:38 +0530

    register,login,logout basic backend done

--- fundpal/main/urls.py
+++ fundpal/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('register/',views.register, name="register"),
-    path('login/',views.login, name="login")
+    path('login/',views.loginPage, name="login"),
+    path('logout/',views.logoutPage, name="logout")
 ]
+
--- fundpal/main/views.py
+++ fundpal/main/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'main/home.html')
 
 def register(request):
-    context = {}
+    form = RegisterForm()
+    if request.method=='POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account was created for ' + form.cleaned_data.get('username'))
+            return redirect('login')
+
+    context = {'form':form}
     return render(request, 'main/register.html', context)
 
-def login(request):
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+            return render(request, 'main/login.html')
+        
     context = {}
     return render(request, 'main/login.html', context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('home')
