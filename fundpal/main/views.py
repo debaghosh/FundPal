f10b7a2f1c2f2e662638a5980332b97ec0ca8821
debaghosh <debanjanaghosh99@gmail.com>
2020-10-02 10:59:13 +0530

    added captcha to login

--- fundpal/main/views.py
+++ fundpal/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+import requests
+import json
 
 def home(request):
     return render(request, 'main/home.html')
@@ -23,6 +25,17 @@ def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        cap_token = request.POST.get('g-recaptcha-response')
+        
+        cap_url = "https://www.google.com/recaptcha/api/siteverify"
+        cap_secret = "6LdhKdIZAAAAAAiJ42ywngqbCAacEqtgmI-e_IPc"
+        cap_dict = {'secret': cap_secret,'response':cap_token}
+        cap_server_response = requests.post(url=cap_url, data=cap_dict)
+        cap_json = json.loads(cap_server_response.text)
+        if cap_json["success"]==False:
+            messages.error(request, 'Captcha incorrect. Try again')
+            return render(request, 'main/login.html')
+
         user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
